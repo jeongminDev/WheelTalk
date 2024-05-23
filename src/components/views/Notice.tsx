45e91581907653c7e6e19e6ef25f8b0b0c7a20fb
JeongMin_Byeon <bjm4256@gmail.com>
2024-05-23 18:45:23 +0900

    댓글이 있을 때만 보이게

--- src/components/views/Notice.tsx
+++ src/components/views/Notice.tsx
@@ -151,42 +151,44 @@ const Notice = () => {
           <button type="submit">{isLoading ? '등록중..' : '댓글 등록'}</button>
         </CommentForm>
       </ArticleContainer>
-      <CommentWrap>
-        <h3 className="font-bold text-xl">Comment</h3>
-        {comments.map((comment) => (
-          <CommentItem key={comment.id}>
-            <div>
-              <span>
-                <svg
-                  fill="none"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                </svg>
-                {comment.username}
-              </span>
-              <span>{formatDate(comment.createdAt)}</span>
-            </div>
-            <div>
-              <p>{comment.content}</p>
-              {comment.userId === user?.uid && (
-                <>
-                  <button>수정</button>
-                  <button onClick={() => onDelete(comment.id)}>삭제</button>
-                </>
-              )}
-            </div>
-          </CommentItem>
-        ))}
-      </CommentWrap>
+      {comments.length > 0 && (
+        <CommentWrap>
+          <h3 className="font-bold text-xl">Comment</h3>
+          {comments.map((comment) => (
+            <CommentItem key={comment.id}>
+              <div>
+                <span>
+                  <svg
+                    fill="none"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                  {comment.username}
+                </span>
+                <span>{formatDate(comment.createdAt)}</span>
+              </div>
+              <div>
+                <p>{comment.content}</p>
+                {comment.userId === user?.uid && (
+                  <>
+                    <button>수정</button>
+                    <button onClick={() => onDelete(comment.id)}>삭제</button>
+                  </>
+                )}
+              </div>
+            </CommentItem>
+          ))}
+        </CommentWrap>
+      )}
     </ArticleWrap>
   );
 };
